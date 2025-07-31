bad420b17afe692ffb2083084d2f58ac492b837f
LordMorgana <morganbleasdale234@gmail.com>
2025-07-31 22:09:22 +0100

    added anims

--- src/components/anim-link.jsx
+++ src/components/anim-link.jsx
@@ -5,7 +5,14 @@ export default function AnimLink ({children}) {
     <motion.div
       className="relative inline-block cursor-pointer"
       whileHover="hover"
-      initial="initial"
+      initial={{ opacity: 0, y: 50, scale: 0.8 }}
+      animate={{ opacity: 1, y: 0, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        ease: "easeOut",
+        delay: 0.1,
+      }}
+
     >
       <span className="lg:text-xl text-lg font-normal text-[#e9ebf0]">{children}</span>
 
